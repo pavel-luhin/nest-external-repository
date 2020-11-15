8709f4d58c8791777f22f3ef315893529b564da5
Pavel Luhin <pavel.luhin@moneyman.ru>
2020-11-15 23:14:42 +0300

    add @EntityRepository decorator

--- app/src/app.repository.ts
+++ app/src/app.repository.ts
@@ -1,6 +1,8 @@
 import {AppEntity} from "./app.entity";
 import {SimpleRepository} from "common-repo/dist/simple.repository";
+import {EntityRepository} from "typeorm";
 
+@EntityRepository(AppEntity)
 export class AppRepository extends SimpleRepository<AppEntity> {
 
 }
