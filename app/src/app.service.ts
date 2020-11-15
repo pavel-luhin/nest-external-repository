316784475a406a06949fda54d55491cd0e37c819
Jay McDoniel <jmcdo29@gmail.com>
2020-11-15 12:49:35 -0800

    feat(jmcdo): postbuild to allow for development

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import {AppRepository} from "./app.repository";
 
 @Injectable()
 export class AppService {
 
   constructor(
+    @InjectRepository(AppRepository)
       private readonly repository: AppRepository
   ) {
   }
